b605a865520701c6f07b223a3ca5c4edf5516533
MohammedSallout <msallout294@gmail.com>
2023-04-12 14:37:46 +0300

    change functions Relates #28

---
+++ public/js/valid.js
@@ -0,0 +1,80 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const logBtn = document.getElementById('log-btn');
+const error_meesages = document.querySelector('.error_meesage');
+
+
+class ValidationError extends Error {
+  constructor(message, fieldName) {
+    super(message);
+    this.fieldName = fieldName;
+  }
+}
+const loginSchema = joi.object({
+  email: joi
+    .string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "org"] } })
+    .required()
+    .error(
+      () =>
+        new ValidationError(
+          "Use a working email address, so you can receive our messages!",
+          "email"
+        )
+    ),
+  password: joi
+    .string()
+    .min(5)
+    .max(30)
+    .required()
+    .error(
+      () =>
+        new ValidationError(
+          "Your password is too easy to guess: you can improve it by adding additional uppercase letters, lowercase letters, or numbers.",
+          "password"
+        )
+    ),
+});
+
+logBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email_text = email.value;
+  const password_text = password.value;
+  const { error } = loginSchema.validate({
+    email: email_text,
+    password: password_text,
+  });
+
+  if (error) {
+    if (error.fieldName === "email") {
+      email_error.classList.remove("error_meesage_hidden");
+      email_error.textContent = error.message;
+    } else if (error.fieldName === "password") {
+      password_error.classList.remove("error_meesage_hidden");
+      password_error.textContent = error.message;
+    }
+  } else {
+    error_meesages.forEach((item) => {
+      item.classList.add("error_meesage_hidden");
+    });
+    // send the data to the backend
+    fetch("/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email_text,
+        password: password_text,
+      }),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        if(data.message === "login sccusseful"){
+          window.location.href = "/"
+        }
+      })
+      .catch((error) => {
+        console.log("error!!!", error);
+      });
+  }
+});
--- server/router/router.js
+++ server/router/router.js
@@ -3,11 +3,19 @@ const router = require('express').Router();
 
 const{ getPostsController, addPostController, addUserController} = require('../controllers')
 
+const loginController = require('../controllers/users/loginUser')
+
 router.get('/signup', (req, res)=>{
  res.status(200).sendFile(path.join(__dirname,'..','..', 'public','control','signup.html' ));
 })
 
+router.get('/login', (req, res)=>{
+  res.status(200).sendFile(path.join(__dirname,'..','..', 'public','control','login.html' ));
+ })
+
 router.post('/signup', addUserController)
+
+router.post('/login' , loginController)
  
 router.get('/posts', getPostsController)
 
