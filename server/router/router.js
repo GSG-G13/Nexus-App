ef673c108a43be2042931bba2f621f17368c262a
heleena-fadi <miss.heleena98@gmail.com>
2023-04-12 14:49:21 +0300

    editing on error handle Relates #35

--- server/router/router.js
+++ server/router/router.js
@@ -7,14 +7,13 @@ const{ getPostsController, addPostController, addUserController} = require('../c
 router.get('/signup', (req, res)=>{
  res.status(200).sendFile(path.join(__dirname,'..','..', 'public','control','signup.html' ));
 })
-router.use(clientError);
-router.use(serverError);
 
 
 router.post('/signup', addUserController)
- 
+
 router.get('/posts', getPostsController)
 
 router.post('/posts', addPostController)
+
 
 module.exports = router;
